f69247fd5e5392bc5c6e9a0a7cdfc816b0fa8e08
Giacobrine <146070359+Giacobrine@users.noreply.github.com>
2023-11-12 11:18:08 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMember() {
+    return {
+        restrict: 'E',
+        templateUrl: 'app/components/member/skills.html',
+        controller: 'SkillsMemberController',
+        controllerAs: 'skillsMemberCtrl'
+    };
+}
